06166c5ef0dfb86d0e185ec9ca3c0999f8762933
dflickinger <dflickinger@altavian.com>
2017-02-21 17:44:11 -0500

    Section 1 Lecture 16 complete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import API_KEY from '../secrets';
+import SearchBar from './components/search_bar';
 
 //Create a new component.
 //This component should produce some HTML.
+//this is called a functional component as opposed to a class component
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 }
 //Take this component's generated HTML and render it in the DOM
 ReactDOM.render(<App/>, document.querySelector('.container'));
