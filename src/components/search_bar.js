4fab476b63264d2702f4f6021db0b7fcee664b0c
DevinFlick <DevinJFlick@gmail.com>
2017-02-22 23:52:51 -0500

    Lesson 24

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,30 +1,51 @@
-// this import syntax is is just the same as doing:
-//const Component = React.Component;
 import React, { Component } from 'react';
 
-//class components, using ES6 class feature, are used when you want a component to have some kind of internal record keeping
-//by extending the React.Component class we capture all the functionality that inheritly comes with react
-//"class SearchBar extends React.Component" { becomes class SearchBar extends Component { thanks to our import syntax
 class SearchBar extends Component {
-//render is a method on the React.Component that must return some JSX or an error will throw
-  render(){
-    //onChange is a react specific event keyword, react docs have more info
-    return <input onChange={this.onInputChange}/>
-    //ES6 syntax allows for the event handler to be ditched and an arrow function to be used instead:
-    //return <input onChange={(event) => console.log(event.target.value)}/>
-    //if you have a single argument you can drop the parameter/args pararentheses as well
-    //return <input onChange={event => console.log(event.target.value)}/>
-  }
+  constructor(props){
+    super(props);
 
-//this is the event handler for the onChange above
-  onInputChange(event){
-    console.log(event.target.value);
+    this.state = { term: '' };
+  }
+  render(){
+    return (
+      <div>
+        <input
+        value = { this.state.term }
+        onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
-//functional component
-// const SearchBar = () => {
-//   return <input/>
-// };
+export default SearchBar;
 
-export default SearchBar
+// // this import syntax is is just the same as doing:
+// //const Component = React.Component;
+// import React, { Component } from 'react';
+//
+// //class components, using ES6 class feature, are used when you want a component to have some kind of internal record keeping
+// //by extending the React.Component class we capture all the functionality that inheritly comes with react
+// //"class SearchBar extends React.Component" { becomes class SearchBar extends Component { thanks to our import syntax
+// class SearchBar extends Component {
+// //render is a method on the React.Component that must return some JSX or an error will throw
+//   render(){
+//     //onChange is a react specific event keyword, react docs have more info
+//     return <input onChange={this.onInputChange}/>
+//     //ES6 syntax allows for the event handler to be ditched and an arrow function to be used instead:
+//     //return <input onChange={(event) => console.log(event.target.value)}/>
+//     //if you have a single argument you can drop the parameter/args pararentheses as well
+//     //return <input onChange={event => console.log(event.target.value)}/>
+//   }
+//
+// //this is the event handler for the onChange above
+//   onInputChange(event){
+//     console.log(event.target.value);
+//   }
+// }
+//
+// //functional component
+// // const SearchBar = () => {
+// //   return <input/>
+// // };
+//
+// export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import API_KEY from '../secrets';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-//Create a new component.
-//This component should produce some HTML.
-//this is called a functional component as opposed to a class component
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+  YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+      return (
+        <div>
+          <SearchBar />
+          <VideoList videos={this.state.videos} />
+        </div>
+    );
+  }
 }
-//Take this component's generated HTML and render it in the DOM
+
 ReactDOM.render(<App/>, document.querySelector('.container'));
 
-//d
+
+// //Create a new component.
+// //This component should produce some HTML.
+// //this is called a functional component as opposed to a class component
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar/>
+//     </div>
+//   );
+// }
+// //Take this component's generated HTML and render it in the DOM
+// ReactDOM.render(<App/>, document.querySelector('.container'));
+//
